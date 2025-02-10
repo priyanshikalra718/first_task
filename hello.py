7099aad208bfd7d3c1d7b367f9625305e48f70de
Priyanshi Kalra <priyanshikalra718@gmail.com>
2025-02-10 10:36:35 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ if num < 0:
 elif num == 0:
    print("The factorial of 0 is 1")
 else:
-   for (i=1;i<=n;i++):
+   for i in range(1,num+1):
        fact = fact*i
    print("The factorial of",num,"is",fact)
 
